7f89a473a509bb708a27efce9b93b3e316aa20f4
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-12-06 02:18:34 +0900

    add first

---
+++ src/org/fukata/android/addeluxe/example/AdDeluxeExampleActivity.java
@@ -0,0 +1,13 @@
+package org.fukata.android.addeluxe.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AdDeluxeExampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
