1a61d01639027bd91eac81068691defb8e4dd172
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2012-01-18 01:25:56 +0900

    use sdk

--- src/org/fukata/android/addeluxe/example/AdDeluxeExampleActivity.java
+++ src/org/fukata/android/addeluxe/example/AdDeluxeExampleActivity.java
@@ -1,5 +1,7 @@
 package org.fukata.android.addeluxe.example;
 
+import org.fukata.android.addeluxe.AdDeluxeAdView;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
